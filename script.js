807027789cf36bcd2e2ef377ded470095cbc7819
MaitriDA <aminmaitri2001@gmail.com>
2021-05-12 00:00:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const quotes=document.getElementById('quotes');
+const author=document.getElementById('author');
+const newBtn=document.getElementById('newBtn');
+
+let orgData="";
+let quoteData="";
+//Generating Random Number
+const   getNewQuotes=()=>{
+    let rnum=Math.floor(Math.random()*1643);
+    quoteData=orgData[rnum];
+    quotes.innerText=`${orgData[rnum].text}`;
+    quoteData.author==null
+        ?(author.innerText='UnKnown')
+        :(author.innerText=`${orgData[rnum].author}`);
+}
+const getQuotes=async () =>{
+    const api="https://type.fit/api/quotes";
+    try{
+        let data=await fetch(api);
+        orgData=await data.json();
+
+        getNewQuotes();
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+newBtn.addEventListener('click',getNewQuotes);
+getQuotes();
