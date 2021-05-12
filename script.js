7603fc674653d8ea7feb08fc5d5949fd5952dbe8
MaitriDA <aminmaitri2001@gmail.com>
2021-05-12 21:29:34 +0530

    enabled sharing feature

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
+let orgData="";
+let quoteData="";
+let data="";
+let dataAut="";
 const quotes=document.getElementById('quotes');
 const author=document.getElementById('author');
 const newBtn=document.getElementById('newBtn');
 
-let orgData="";
-let quoteData="";
+const image=document.createElement('img');
+image.className='image';
+
+const twitter=document.getElementById('twitter');
+
+const whatsapp=document.getElementById('whatsapp');
+
+const sharewhatsapp=()=>{
+    let whatsappPost=`https://api.whatsapp.com/send?text=${data} By ${dataAut}`;
+    window.open(whatsappPost)
+}
+
+// const tweetNow=()=>{
+//     let tweetPost=`https://twitter.com/intent/tweet?text=${data} By ${dataAut}`;
+//     window.open(tweetPost)
+// }
+
+const tweetNow=()=>{
+    let tweetPost=`https://www.linkedin.com/shareArticle?url=${'https://fontawesome.com/icons/whatsapp?style=brands'}&title=${'Hello'}`;
+    window.open(tweetPost)
+}
 //Generating Random Number
 const   getNewQuotes=()=>{
+    let rimage=Math.floor(Math.random()*32);
+    console.log(rimage);
+    image.src=`./images/${rimage}.jpg`;
+    document.querySelector('.mainCard').appendChild(image)
     let rnum=Math.floor(Math.random()*1643);
     quoteData=orgData[rnum];
+    data=orgData[rnum].text;
     quotes.innerText=`${orgData[rnum].text}`;
     quoteData.author==null
         ?(author.innerText='- UnKnown')
         :(author.innerText=`- ${orgData[rnum].author}`);
+    quoteData.author==null
+        ?(dataAut='UnKnown')
+        :(dataAut=orgData[rnum].author);
 }
 const getQuotes=async () =>{
     const api="https://type.fit/api/quotes";
@@ -26,5 +57,10 @@ const getQuotes=async () =>{
     }
 };
 
+twitter.addEventListener('click',tweetNow);
+whatsapp.addEventListener('click',sharewhatsapp)
 newBtn.addEventListener('click',getNewQuotes);
 getQuotes();
+
+
+
